5f8d4d57b6367cb356773c34100b49fc6aabce40
Luna <0510winnie@gmail.com>
2019-04-17 16:50:50 +0800

    update route

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//List articles
-Route::resource('todos', 'TodoController');
+// todos
+Route::apiResource('todos', 'TodoController');
